9e8d1aafe619ba503f71ee01e55e43ea9a50abfe
Tomek <tomaszmularczyk89@gmail.com>
2017-12-18 15:18:30 +0100

    fixed flow

--- src/components/AuthorizedRoute.jsx
+++ src/components/AuthorizedRoute.jsx
@@ -2,42 +2,45 @@ import React from 'react'
 import { Redirect, Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 
-const AuthorizedRoute = ({ component: Component, path, isProfileCreated, getNextStep, ...rest }) => (
-  <Route {...rest} path={path} render={props => {
-    const nextStep = getNextStep()
-    if (isProfileCreated || path === nextStep) {
-      return <Component {...props} />
+const AuthorizedRoute = ({ component: Component, isProfileCreated, isEmailConfirmed, isLoggedIn, ...rest }) => (
+  <Route {...rest} render={props => {
+    // if user is fully registered - grant him every route except registration steps.
+    if (isProfileCreated) {
+      if (['/', '/create-profile', '/confirm-email'].includes(props.location.pathname)) {
+        return <Redirect to="/dashboard" />
+      } else {
+        return <Component {...props} />
+      }
     }
 
-    return <Redirect to={nextStep} />
+    // user is not fully registered so he needs to be redirected to next step
+    if (isEmailConfirmed) {
+      if (props.location.pathname === '/create-profile') {
+        return <Component {...props} />
+      } else {
+        return <Redirect to="/create-profile" />
+      }
+    }
+
+    if (isLoggedIn) {
+      if (props.location.pathname === '/confirm-email') {
+        return <Component {...props} />
+      } else {
+        return <Redirect to="/confirm-email" />
+      }
+    }
+
+    if (props.location.pathname === '/') {
+      return <Component {...props} />
+    }
+    return <Redirect to="/" />
   }} />
 )
 
 export default connect(
-  (state, ownProps) => ({
+  (state) => ({
     isProfileCreated: state.isProfileCreated,
-    getNextStep: () => {
-      if (state.isProfileCreated) {
-        return ownProps.path // allow access to any route
-        /**
-
-         Sometimes after user has been successfully logged we want to make manual redirect from registration-pages
-         to something like landing page or dashboard:
-
-         if (['/', '/create-profile', '/confirm-email'].includes(ownProps.path)) {
-           return '/dashboard';
-         }
-
-         * */
-      }
-      if (state.isEmailConfirmed) {
-        return '/create-profile'
-      }
-      if (state.isLoggedIn) {
-        return '/confirm-email'
-      }
-
-      return '/'
-    }
+    isEmailConfirmed: state.isEmailConfirmed,
+    isLoggedIn: state.isLoggedIn,
   }),
 )(AuthorizedRoute)
